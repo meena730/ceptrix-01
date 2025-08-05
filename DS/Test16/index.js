e706dffcc80786776b37194c92aa1e8d0221cbbc
Meena Dhakad <dhakadm970@gmail.com>
2025-08-05 10:49:47 +0530

    add subheading behalf of free delivery

---
+++ DS/Test16/index.js
@@ -0,0 +1,96 @@
+function waitForElement(selector, callback, interval = 50, timeout = 10000) {
+  const check = setInterval(() => {
+    const els = document.querySelectorAll(selector);
+    if (els.length) {
+      clearInterval(check);
+      callback(els);
+    }
+  }, interval);
+  setTimeout(() => clearInterval(check), timeout);
+}
+
+if (window.location.href === "https://schroevendump.nl/winkelwagen/") {
+  document.body.classList.add("gmd-001")
+  document.querySelectorAll(".order-total-novat").forEach((el) => {
+    el.style.display = "none";
+  });
+
+  const heading = document.querySelector(".cart-collaterals .order-total h6");
+  if (heading) {
+    heading.textContent = "Subtotaal (excl. btw)";
+  }
+
+  waitForElement(
+    ".cart-collaterals .order-total .woocommerce-Price-amount",
+    ([priceEl]) => {
+      const rawPrice = priceEl.textContent
+        .replace(/[^\d,]/g, "")
+        .replace(",", ".");
+      const total = parseFloat(rawPrice);
+      if (isNaN(total)) return;
+
+      const euro = (num) => `€ ${num.toFixed(2).toString().replace(".", ",")}`;
+      const newRow = document.createElement("div");
+
+      if (total < 99) {
+        const shipping = 7.5;
+        const finalTotal = total + shipping;
+
+        const shippingRow = document.createElement("div");
+        shippingRow.className = "gmd-total";
+        shippingRow.innerHTML = `
+          <h6 style="font-weight: normal;">Verzendkosten</h6>
+          <div data-title="Verzending">
+            <span class="woocommerce-Price-amount amount"><bdi>${euro(
+              shipping
+            )}</bdi></span>
+          </div>
+        `;
+
+        const totalRow = document.createElement("div");
+        totalRow.className = "gmd-total";
+        totalRow.innerHTML = `
+          <h6 style="font-weight: normal;">Subtotaal (Incl. btw)</h6>
+          <div data-title="Totaal incl.">
+            <span class="woocommerce-Price-amount amount"><bdi>${euro(
+              finalTotal
+            )}</bdi></span>
+          </div>
+        `;
+
+        newRow.appendChild(shippingRow);
+        newRow.appendChild(totalRow);
+      } else {
+        const shippingRow = document.createElement("div");
+        shippingRow.className = "order-total";
+        shippingRow.innerHTML = `
+          <h6 style="font-weight: normal;">Verzendkosten</h6>
+          <div data-title="Verzending">
+            <span class="woocommerce-Price-amount amount" style="color: green; font-weight: normal;">Gratis</span>
+          </div>
+        `;
+
+        const totalRow = document.createElement("div");
+        totalRow.className = "order-total";
+        totalRow.innerHTML = `
+          <h6 style="font-weight: normal;">Subtotaal (Incl. btw)</h6>
+          <div data-title="Totaal incl.">
+            <span class="woocommerce-Price-amount amount" style="font-weight: normal;"><bdi>${euro(
+              total
+            )}</bdi></span>
+          </div>
+        `;
+
+        newRow.appendChild(shippingRow);
+        newRow.appendChild(totalRow);
+      }
+
+      const totalBlock = document.querySelector(
+        ".cart-collaterals .order-total"
+      );
+      if (totalBlock) {
+        totalBlock.insertAdjacentElement("afterend", newRow);
+      }
+    }
+  );
+}
