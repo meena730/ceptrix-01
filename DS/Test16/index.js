bff8e6d70234003f369f8dea6e6d24c62005e3e9
Meena Dhakad <dhakadm970@gmail.com>
2025-08-08 12:11:10 +0530

    formate currecncy

--- DS/Test16/index.js
+++ DS/Test16/index.js
@@ -21,80 +21,78 @@ if (window.location.href === "https://schroevendump.nl/winkelwagen/") {
     heading.style.color = "#70707b";
     // heading.classList.add= "subtotal-exl"
   }
- 
-  
 
-  waitForElement(
-    ".cart-collaterals .order-total .woocommerce-Price-amount",
-    ([priceEl]) => {
-      const rawPrice = priceEl.textContent
-        .replace(/[^\d,]/g, "")
-        .replace(",", ".");
-      const total = parseFloat(rawPrice);
-      if (isNaN(total)) return;
+ waitForElement(
+   ".cart-collaterals .order-total .woocommerce-Price-amount",
+   ([priceEl]) => {
+     const rawPrice = priceEl.textContent
+       .replace(/[^\d,]/g, "")
+       .replace(",", ".");
+     const total = parseFloat(rawPrice);
+     if (isNaN(total)) return;
 
-      const euro = (num) => `€ ${num.toFixed(2).toString().replace(".", ",")}`;
-      const newRow = document.createElement("div");
+     const euro = (num) =>
+       new Intl.NumberFormat("nl-NL", {
+         style: "currency",
+         currency: "EUR",
+       }).format(num);
 
-      if (total < 99) {
-        const shipping = 7.5;
-        const finalTotal = total + shipping;
+     const newRow = document.createElement("div");
 
-        const shippingRow = document.createElement("div");
-        shippingRow.className = "gmd-total";
-        shippingRow.innerHTML = `
-          <h6>Verzendkosten</h6>
-          <div>
-            <span class="gmd-amount"><bdi>${euro(
-              shipping
-            )}</bdi></span>
-          </div>
-        `;
+     if (total < 99) {
+       const shipping = 7.5;
+       const finalTotal = total + shipping;
 
-        const totalRow = document.createElement("div");
-        totalRow.className = "gmd-total";
-        totalRow.innerHTML = `
-          <h6>Subtotaal (Incl. btw)</h6>
-          <div>
-            <span class="gmd-amount"><bdi>${euro(
-              total
-            )}</bdi></span>
-          </div>
-        `;
+       const shippingRow = document.createElement("div");
+       shippingRow.className = "gmd-total";
+       shippingRow.innerHTML = `
+        <h6>Verzendkosten</h6>
+        <div>
+          <span class="gmd-amount"><bdi>${euro(shipping)}</bdi></span>
+        </div>
+      `;
 
-        newRow.appendChild(shippingRow);
-        newRow.appendChild(totalRow);
-      } else {
-        const shippingRow = document.createElement("div");
-        shippingRow.className = "gmd-total";
-        shippingRow.innerHTML = `
-          <h6>Verzendkosten</h6>
-          <div>
-            <span class="gmd-amount" style="color: green;">Gratis</span>
-          </div>
-        `;
+       const totalRow = document.createElement("div");
+       totalRow.className = "gmd-total";
+       totalRow.innerHTML = `
+        <h6>Subtotaal (Incl. btw)</h6>
+        <div>
+          <span class="gmd-amount"><bdi>${euro(finalTotal)}</bdi></span>
+        </div>
+      `;
 
-        const totalRow = document.createElement("div");
-        totalRow.className = "gmd-total";
-        totalRow.innerHTML = `
-          <h6>Subtotaal (Incl. btw)</h6>
-          <div>
-            <span class="gmd-amount"><bdi>${euro(
-              total
-            )}</bdi></span>
-          </div>
-        `;
+       newRow.appendChild(shippingRow);
+       newRow.appendChild(totalRow);
+     } else {
+       const shippingRow = document.createElement("div");
+       shippingRow.className = "gmd-total";
+       shippingRow.innerHTML = `
+        <h6>Verzendkosten</h6>
+        <div>
+          <span class="gmd-amount" style="color: green;">Gratis</span>
+        </div>
+      `;
 
-        newRow.appendChild(shippingRow);
-        newRow.appendChild(totalRow);
-      }
+       const totalRow = document.createElement("div");
+       totalRow.className = "gmd-total";
+       totalRow.innerHTML = `
+        <h6>Subtotaal (Incl. btw)</h6>
+        <div>
+          <span class="gmd-amount"><bdi>${euro(total)}</bdi></span>
+        </div>
+      `;
 
-      const totalBlock = document.querySelector(
-        ".cart-collaterals .order-total"
-      );
-      if (totalBlock) {
-        totalBlock.insertAdjacentElement("afterend", newRow);
-      }
-    }
-  );
+       newRow.appendChild(shippingRow);
+       newRow.appendChild(totalRow);
+     }
+
+     const totalBlock = document.querySelector(
+       ".cart-collaterals .order-total"
+     );
+     if (totalBlock) {
+       totalBlock.insertAdjacentElement("afterend", newRow);
+     }
+   }
+ );
+
 }
