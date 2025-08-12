0df9a5f0327e23349e7e6da4e711fb9a62c891cf
Meena Dhakad <dhakadm970@gmail.com>
2025-08-12 16:23:52 +0530

    update style and logic

--- DS/Test18/index.js
+++ DS/Test18/index.js
@@ -18,42 +18,37 @@ if (
   waitForElement(".wijcbf_form_wrapper", (wrapper) => {
     document.body.classList.add("gmd-001");
 
-    function addHeadings() {
-      const dateBlock = document.querySelector(
-        ".wijcbf_form_options_wrapper.dates"
-      );
-      const timeBlock = document.querySelector("#wijcbf_date_row");
+   function addHeadings() {
+     const dateBlock = document.querySelector(
+       ".wijcbf_form_options_wrapper.dates"
+     );
+     const timeBlock = document.querySelector("#wijcbf_date_row");
 
-      if (
-        dateBlock &&
-        !dateBlock.previousElementSibling?.classList.contains("gmd-label-day")
-      ) {
-        dateBlock.insertAdjacentHTML(
-          "beforebegin",
-          `
-          <div class="gmd-label gmd-label-day">
-            <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/Vector_8_sq5tni.png" alt="calendar icon" />
-            <span class="time-text">Kies een dag</span>
-          </div>
-        `
-        );
-      }
+     if (dateBlock && !document.querySelector(".gmd-label-day")) {
+       dateBlock.insertAdjacentHTML(
+         "beforebegin",
+         `
+      <div class="gmd-label gmd-label-day">
+        <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/Vector_8_sq5tni.png" alt="calendar icon" />
+        <span class="time-text">Kies een dag</span>
+      </div>
+      `
+       );
+     }
 
-      if (
-        timeBlock &&
-        !timeBlock.nextElementSibling?.classList.contains("gmd-label-time")
-      ) {
-        timeBlock.insertAdjacentHTML(
-          "afterend",
-          `
-          <div class="gmd-label gmd-label-time section" style="display: none;">
-            <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/mingcute_time-line_qcznp8.png" alt="clock icon" />
-            <span class="time-text">Kies een tijd</span>
-          </div>
-        `
-        );
-      }
-    }
+     if (timeBlock && !document.querySelector(".gmd-label-time")) {
+       timeBlock.insertAdjacentHTML(
+         "afterend",
+         `
+      <div class="gmd-label gmd-label-time section" style="display: none;">
+        <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/mingcute_time-line_qcznp8.png" alt="clock icon" />
+        <span class="time-text">Kies een tijd</span>
+      </div>
+      `
+       );
+     }
+   }
+
 
     function setupDateSelection() {
       const dateButtons = document.querySelectorAll(
