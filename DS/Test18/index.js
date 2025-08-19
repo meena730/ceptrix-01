30df7c3849d20728e0786b45dcab4e45aa51e50a
Meena Dhakad <dhakadm970@gmail.com>
2025-08-19 08:21:26 +0530

    update  class name

--- DS/Test18/index.js
+++ DS/Test18/index.js
@@ -10,52 +10,47 @@ function waitForElement(selector, callback, interval = 50, timeout = 10000) {
   setTimeout(() => clearInterval(timer), timeout);
 }
 
-if (
-  window.location.href.includes(
-    "https://www.rijbewijskeuringofficieel.nl/maak-een-afspraak/"
-  )
-) {
-  waitForElement(".wijcbf_form_wrapper", (wrapper) => {
+if (window.location.href.includes("/maak-een-afspraak/")) {
+  waitForElement('[id*="bookly_form"]', (wrapper) => {
     document.body.classList.add("gmd-001");
 
-   function addHeadings() {
-     const dateBlock = document.querySelector(
-       ".wijcbf_form_options_wrapper.dates"
-     );
-     const timeBlock = document.querySelector("#wijcbf_date_row");
+    function addHeadings() {
+      const dateBlock = document.querySelector(
+        '[class*="options_wrapper"].dates'
+      );
+      const timeBlock = document.querySelector('[id*="date_row"]');
 
-     if (dateBlock && !document.querySelector(".gmd-label-day")) {
-       dateBlock.insertAdjacentHTML(
-         "beforebegin",
-         `
-      <div class="gmd-label gmd-label-day">
-        <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/Vector_8_sq5tni.png" alt="calendar icon" />
-        <span class="time-text">Kies een dag</span>
-      </div>
-      `
-       );
-     }
+      if (dateBlock && !document.querySelector(".gmd-label-day")) {
+        dateBlock.insertAdjacentHTML(
+          "beforebegin",
+          `
+        <div class="gmd-label gmd-label-day">
+          <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/Vector_8_sq5tni.png" alt="calendar icon" />
+          <span class="time-text">Kies een dag</span>
+        </div>
+        `
+        );
+      }
 
-     if (timeBlock && !document.querySelector(".gmd-label-time")) {
-       timeBlock.insertAdjacentHTML(
-         "afterend",
-         `
-      <div class="gmd-label gmd-label-time section" style="display: none;">
-        <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/mingcute_time-line_qcznp8.png" alt="clock icon" />
-        <span class="time-text">Kies een tijd</span>
-      </div>
-      `
-       );
-     }
-   }
-
+      if (timeBlock && !document.querySelector(".gmd-label-time")) {
+        timeBlock.insertAdjacentHTML(
+          "afterend",
+          `
+        <div class="gmd-label gmd-label-time section" style="display: none;">
+          <img class="time-icon" src="https://res.cloudinary.com/diilhbcp9/image/upload/v1754893875/mingcute_time-line_qcznp8.png" alt="clock icon" />
+          <span class="time-text">Kies een tijd</span>
+        </div>
+        `
+        );
+      }
+    }
 
     function setupDateSelection() {
       const dateButtons = document.querySelectorAll(
-        ".wijcbf_date_container .wijcbf_date"
+        '[class*="date_container"] [class*="date"]'
       );
       const timeContainers = document.querySelectorAll(
-        ".wijcbf_flex_container .wijcbf_slot_container"
+        '[class*="flex_container"] [class*="slot_container"]'
       );
       const timeLabel = document.querySelector(".gmd-label-time");
 
