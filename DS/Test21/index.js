7483f47949753cb08cd1ce164b425fdc55d32ddf
Meena Dhakad <dhakadm970@gmail.com>
2025-08-26 11:55:43 +0530

    update code add animation ensure modal show when needed

--- DS/Test21/index.js
+++ DS/Test21/index.js
@@ -13,56 +13,58 @@ if (document.body.classList.contains("product-bundlepage")) {
   document.body.classList.add("gmd-001");
 
   function handleButtons(buttons) {
-   buttons.forEach((button) => {
-  if (button.dataset.gmdBound) return;
-  button.dataset.gmdBound = "true";
+    buttons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const wrapper = button.closest(".gmd-001");
 
-  button.addEventListener("click", () => {
-    setTimeout(() => {
-      waitForElement(
-        ".modal .close-modal.d-block",
-        (closeButtons) => {
-          const closeBtn = closeButtons[0];
-          if (closeBtn) {
-            setTimeout(() => {
-              closeBtn.click();
-            }, 100);
-          }
-        },
-        100,
-        10000
-      );
-
-      if (!button.classList.contains("gmd-checked")) {
-        const originalContent = button.innerHTML;
-
-        button.classList.remove("bg-success", "text-white");
-        button.innerHTML = "";
-
-        const checkIcon = document.createElement("span");
-        checkIcon.className = "gmd-check-icon";
-        checkIcon.innerHTML = `
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="13" viewBox="0 0 18 13" fill="none">
-            <path d="M6.54998 13.0001L0.849976 7.3001L2.27498 5.8751L6.54998 10.1501L15.725 0.975098L17.15 2.4001L6.54998 13.0001Z" fill="#28A745"/>
-          </svg>
-        `;
-        button.appendChild(checkIcon);
-
-        button.classList.add("gmd-checked");
+        if (wrapper) {
+          wrapper.classList.add("modal-disabled");
+        }
 
         setTimeout(() => {
-          button.innerHTML = originalContent;
-          button.classList.remove("gmd-checked");
-          button.classList.add("bg-success", "text-white");
-        }, 2500);
-      }
-    }, 150); 
-  });
-});
+          waitForElement(
+            ".modal .close-modal.d-block",
+            (closeButtons) => {
+              const closeBtn = closeButtons[0];
+              if (closeBtn) {
+                setTimeout(() => {
+                  closeBtn.click();
+                }, 100);
+              }
+            },
+            100,
+            10000
+          );
 
+          if (!button.classList.contains("gmd-checked")) {
+            const originalContent = button.innerHTML;
+
+            button.classList.remove("bg-success", "text-white");
+            button.innerHTML = "";
+
+            const checkIcon = document.createElement("span");
+            checkIcon.className = "gmd-check-icon animate-in";
+            checkIcon.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="13" viewBox="0 0 18 13" fill="none">
+      <path d="M6.54998 13.0001L0.849976 7.3001L2.27498 5.8751L6.54998 10.1501L15.725 0.975098L17.15 2.4001L6.54998 13.0001Z" fill="#28A745"/>
+    </svg>
+  `;
+            button.appendChild(checkIcon);
+
+            button.classList.add("gmd-checked");
+
+            setTimeout(() => {
+              button.innerHTML = originalContent;
+              button.classList.remove("gmd-checked");
+              button.classList.add("bg-success", "text-white");
+            }, 2500);
+          }
+        }, 150);
+      });
+    });
   }
 
-  //  function 
+  //  function
   function handleBundleRows() {
     document.querySelectorAll(".bundle-list").forEach((bundle) => {
       bundle.id = "gmd-bundle";
@@ -184,13 +186,38 @@ if (document.body.classList.contains("product-bundlepage")) {
 
           if (btn && stock) {
             const text = btn.textContent.trim();
+
             if (text === "Andere optie") {
               btn.innerHTML = `
-              Kies andere optie 
-              <img src="https://res.cloudinary.com/diwhc4afs/image/upload/v1756112959/material-symbols_change-circle-outline-rounded_kqyn8q.png" 
-                  alt="icon" >
-            `;
+   <span> Kies andere optie </span>
+    <img 
+      src="https://res.cloudinary.com/diwhc4afs/image/upload/v1756112959/material-symbols_change-circle-outline-rounded_kqyn8q.png" 
+      alt="icon" 
+      
+    >
+  `;
+
+              btn.addEventListener("click", (e) => {
+                e.preventDefault();
+
+                document.body.classList.remove(
+                  "modal-disabled",
+
+                  "modal-open"
+                );
+
+                document
+                  .querySelectorAll(".modal-disabled, .modal-open ")
+                  .forEach((el) =>
+                    el.classList.remove(
+                      "modal-disabled",
+
+                      "modal-open"
+                    )
+                  );
+              });
             }
+
             stock.insertAdjacentElement("afterend", btn);
           }
         });
@@ -204,7 +231,7 @@ if (document.body.classList.contains("product-bundlepage")) {
     }
   );
 
-  // Mutation 
+  // Mutation
   const observer = new MutationObserver(() => {
     const buttons = document.querySelectorAll(
       ".main-container .bundle-list .product-price-add-to-cart button[type='button']"
